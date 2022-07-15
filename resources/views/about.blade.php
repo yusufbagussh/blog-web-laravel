27647bbace5f6b5b83a23ce6758ef6365282bc9f
yusufbagussh <yusuf.herlambang27@gmail.com>
2022-07-15 07:25:14 +0700

    delete all files

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-@section('container')
-    <h1>Halaman About</h1>
-    <h3>{{ $nama }}</h3>
-    <p><?= $email?></p>
-    <img src="img/<?= $image?>" alt="yusufbagus" width="200px" class="img-tumbnail rounded-circle">
-@endsection
